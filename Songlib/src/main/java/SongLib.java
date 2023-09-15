4715458e92048da1a4fa3e58eefc49d51643fdcc
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2023-09-15 12:48:11 -0400

    add App Icon

--- Songlib/src/main/java/SongLib.java
+++ Songlib/src/main/java/SongLib.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import ui.UI;
@@ -27,6 +28,7 @@ public class SongLib extends Application {
         primaryStage.setScene(new Scene(root));
         primaryStage.sizeToScene();
         primaryStage.setTitle("SongLib");
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("app-icon.png")));
         primaryStage.setResizable(false);
         primaryStage.show();
     }
